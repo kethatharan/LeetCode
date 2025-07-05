7fa5cddd42d7dbae2c73289539ba3f948b509729
kethatharan <30690675+kethatharan@users.noreply.github.com>
2025-07-05 11:55:06 +0530

    Time: 324 ms (5.65%), Space: 50.1 MB (6.8%) - LeetHub

---
+++ 0135-candy/0135-candy.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Candy(int[] ratings) {
+        int[] can=new int[ratings.Length];
+        for(int i=0;i<ratings.Length;i++){
+            can[i]=1;
+        }
+        for(int i=1;i<ratings.Length;i++){
+            if(ratings[i]>ratings[i-1]){
+                can[i]=can[i-1]+1;
+            }
+        }
+        for(int i=ratings.Length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                can[i]=Math.Max(can[i],can[i+1]+1);
+                
+            }
+        }
+        int sum=0;
+        foreach(var c in can){
+            sum+=c;
+            Console.WriteLine(c);
+        }
+        return sum;
+    }
+}
