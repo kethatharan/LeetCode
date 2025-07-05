035321d48c1024b027776e42d16e857df0c3cce9
kethatharan <30690675+kethatharan@users.noreply.github.com>
2025-07-05 12:53:02 +0530

    Time: 1 ms (78.92%), Space: 70.6 MB (48.24%) - LeetHub

--- 0134-gas-station/0134-gas-station.cs
+++ 0134-gas-station/0134-gas-station.cs
@@ -2,29 +2,24 @@ public class Solution {
     public int CanCompleteCircuit(int[] gas, int[] cost) {
         int start=0;
         int co=gas.Length;
+        int ga=0;
+        int cos=0;
         for(int i=0;i<co;i++){
-            if(gas[i]>cost[i]){
-                start=i;
-                break;
+            ga+=gas[i];
+            cos+=cost[i];
+        }
+        if(cos>ga){
+            return -1;
+        }
+        int avail=0;
+        int ind=0;
+        for(int i=0;i<co;i++){
+            avail=(avail+gas[i])-cost[i];
+            if(avail<0){
+                ind=i+1;
+                avail=0;
             }
         }
-        int tank=0;
-        for(int i=start;i<co;i++){
-            tank=(tank+gas[i])-cost[i];
-            
-        }
-        for(int i=0;i<start;i++){
-            Console.WriteLine(tank+"t");
-            Console.WriteLine(gas[i]);
-            Console.WriteLine(cost[i]);
-           
-            tank=(tank+gas[i])-cost[i];
-        }
-        if(tank==0){
-        return start;
-        }else{
-            return tank;
-        }
-        //return -1;
+        return ind;
     }
 }
